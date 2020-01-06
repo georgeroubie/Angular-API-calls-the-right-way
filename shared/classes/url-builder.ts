b5fdbad6891ad7535b4d624206ae696aba52f6cb
George Roubie <george.roubie@gmail.com>
2020-01-06 11:33:23 +0200

    Create url-builder.ts

---
+++ shared/classes/url-builder.ts
@@ -0,0 +1,17 @@
+// Application Classes
+import { QueryStringParameters } from './query-string-parameters';
+
+export class UrlBuilder {
+    public url: string;
+    public queryString: QueryStringParameters;
+
+    constructor(private baseUrl: string, private action: string, queryString?: QueryStringParameters) {
+        this.url = [baseUrl, action].join('/');
+        this.queryString = queryString || new QueryStringParameters();
+    }
+
+    public toString(): string {
+        const qs: string = this.queryString ? this.queryString.toString() : '';
+        return qs ? `${this.url}?${qs}` : this.url;
+    }
+}
