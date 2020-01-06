71d7748ae74b55292903abc42b7424eff78d3440
George Roubie <george.roubie@gmail.com>
2020-01-06 12:13:22 +0200

    Update constants.ts

--- config/constants.ts
+++ config/constants.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    public readonly API_ENDPOINT: string = 'https://angularwebappapi-dev.azurewebsites.net/api';
+    public readonly API_ENDPOINT: string = 'https://domain.com/api';
     public readonly API_MOCK_ENDPOINT: string = 'https://next.json-generator.com/api/json/get';
 }
